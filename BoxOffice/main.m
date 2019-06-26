eaffa2baa588d38ef8e7af9f5e22ac1526e300dc
Austin <austin@tag219877.local>
2019-06-26 14:42:04 -0500

    switch statements

--- BoxOffice/main.m
+++ BoxOffice/main.m
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[]) {
     //Regular Price $10
     //Senior Price $5
     //matinee Price $4
-    
+    /*
     bool isMatinee = FALSE;
     
     float regPrice = 10;
@@ -41,7 +41,51 @@ int main(int argc, const char * argv[]) {
     {
         custPrice = regPrice;
     }
+    */
     
+    enum popcornSizes{
+        kidsPopcorn     = 1,
+        smallPopcorn    = 2,
+        mediumPopcorn   = 3,
+        largePopcorn    = 4,
+        tubPopcorn      = 5
+    };
+    
+    //popcorn Sizes and prices
+    //kids - $1.50
+    //small - $3.00
+    //medium - $4.25
+    //large - $5.25
+    //tub - $6.00
+    
+    float popcornPrice;
+    int popcornSize;
+    
+    switch (popcornSize = tubPopcorn) {
+        case kidsPopcorn:
+            popcornPrice = 1.50;
+            break;
+            
+        case smallPopcorn:
+            popcornPrice = 3.00;
+            break;
+            
+        case mediumPopcorn:
+            popcornPrice = 4.25;
+            break;
+            
+        case largePopcorn:
+            popcornPrice = 5.25;
+            break;
+            
+        case tubPopcorn:
+            popcornPrice = 6.00;
+            break;
+            
+        default:
+            NSLog(@"No valid size entered");
+            break;
+    }
     
     
     return 0;
